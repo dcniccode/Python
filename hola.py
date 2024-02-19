a0bf918080cf8cd983d548a42157cde119197ae0
Daniela <danielacadenavillanueva@gmail.com>
2024-02-19 11:05:38 -0500

    Este es mi primer commit

---
+++ hola.py
@@ -0,0 +1,10 @@
+
+for letra in "Hola":
+    print(letra)
+print("")
+
+#Imprimir numeros solo numeros pares en un rango
+for i in range(11):
+        print(i)    
+nombre = ["Tony" , "Ziva" ,  "Tim" , "Abby"]
+print(nombre)
